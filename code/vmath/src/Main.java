502a5fb0897a04bb6c0251cc24f41bf2c55b1e13
sakhadib <adibsakhawat@iut-dhaka.edu>
2023-09-09 00:21:59 +0600

    matrix multiplication using general and srassen algorithm

--- code/vmath/src/Main.java
+++ code/vmath/src/Main.java
@@ -1,15 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[][] arr1 = {{1}, {3, 4}};
-        int[][] arr2 = {{5, 6}, {7, 8}};
+        int[][] arr1 = {{1, 2, 7},
+                        {4, 5, 6}};
+
+        int[][] arr2 = {{3, 4},
+                        {6, 5},
+                        {7, 8}};
 
         Matrix matrix1 = new Matrix(arr1);
         Matrix matrix2 = new Matrix(arr2);
 
         matrix1.printMatrix();
+        System.out.println();
         matrix2.printMatrix();
-
-        vmath.addMatrix(matrix1, matrix2).printMatrix();
+        System.out.println();
+        System.out.println();
+        System.out.println("Strassen Algo:");
+        vmath.multiplyMatrix(matrix1, matrix2).printMatrix();
+        System.out.println();
+        System.out.println("General multiplication:");
+        matrix1.multiplyGeneral(matrix2).printMatrix();
     }
 }
--- code/vmath/src/vmath.java
+++ code/vmath/src/vmath.java
@@ -15,6 +15,7 @@ public class vmath {
 
     /**
      * Subtract two matrices
+     * (matrix1 - matrix2)
      * @param matrix1 first matrix
      * @param matrix2 second matrix
      * @return result matrix
@@ -24,6 +25,107 @@ public class vmath {
             throw new IllegalArgumentException("Matrix dimensions must match for subtraction.");
         }
         return matrix1.subtract(matrix2);
+    }
+
+    /**
+     * Multiply two matrices
+     * @param matrix1 first matrix (left)
+     *                (m x n)
+     *                m = rows
+     *                n = cols
+     *                (m x n) * (n x p) = (m x p)
+     * @param matrix2 second matrix (right)
+     *                (n x p)
+     *                p = cols
+     *                (m x n) * (n x p) = (m x p)
+     *                n must equal n
+     * @return result matrix
+     */
+    public static Matrix multiplyMatrix(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.NumberOfCols() != matrix2.NumberOfRows()) {
+            throw new IllegalArgumentException("Matrix dimensions are not compatible for multiplication.");
+        }
+        if (matrix1.NumberOfCols() == matrix1.NumberOfRows() && matrix2.NumberOfCols() == matrix2.NumberOfRows()) {
+            int numRows1 = matrix1.NumberOfRows();
+            int numCols1 = matrix1.NumberOfCols();
+            int numRows2 = matrix2.NumberOfRows();
+            int numCols2 = matrix2.NumberOfCols();
+
+            Matrix modifiedMatrix1 = new Matrix(numRows1 + 1, numCols1 + 1);
+            Matrix modifiedMatrix2 = new Matrix(numRows2 + 1, numCols2 + 1);
+
+            // Copy values from matrix1 to modifiedMatrix1
+            for (int i = 0; i < numRows1; i++) {
+                for (int j = 0; j < numCols1; j++) {
+                    modifiedMatrix1.setValue(i, j, matrix1.getValue(i, j));
+                }
+            }
+
+            // Fill the extra row and column of modifiedMatrix1 with '0' values
+            for (int i = 0; i <= numRows1; i++) {
+                modifiedMatrix1.setValue(i, numCols1, 0);
+            }
+            for (int j = 0; j <= numCols1; j++) {
+                modifiedMatrix1.setValue(numRows1, j, 0);
+            }
+
+            // Copy values from matrix2 to modifiedMatrix2
+            for (int i = 0; i < numRows2; i++) {
+                for (int j = 0; j < numCols2; j++) {
+                    modifiedMatrix2.setValue(i, j, matrix2.getValue(i, j));
+                }
+            }
+
+            // Fill the extra row and column of modifiedMatrix2 with '0' values
+            for (int i = 0; i <= numRows2; i++) {
+                modifiedMatrix2.setValue(i, numCols2, 0);
+            }
+            for (int j = 0; j <= numCols2; j++) {
+                modifiedMatrix2.setValue(numRows2, j, 0);
+            }
+
+            return checkAndtrimMatrix(modifiedMatrix1.multiply(modifiedMatrix2));
+        }
+        return matrix1.multiply(matrix2);
+    }
+
+    private static Matrix checkAndtrimMatrix(Matrix inputMatrix) {
+        int numRows = inputMatrix.NumberOfRows();
+        int numCols = inputMatrix.NumberOfCols();
+
+        // Check if the last row contains all zeros
+        boolean lastRowAllZeros = true;
+        for (int j = 0; j < numCols; j++) {
+            if (inputMatrix.getValue(numRows - 1, j) != 0) {
+                lastRowAllZeros = false;
+                break;
+            }
+        }
+
+        // Check if the last column contains all zeros
+        boolean lastColumnAllZeros = true;
+        for (int i = 0; i < numRows; i++) {
+            if (inputMatrix.getValue(i, numCols - 1) != 0) {
+                lastColumnAllZeros = false;
+                break;
+            }
+        }
+
+        // If both the last row and last column have all zeros, trim them
+        if (lastRowAllZeros && lastColumnAllZeros) {
+            Matrix trimmedMatrix = new Matrix(numRows - 1, numCols - 1);
+
+            for (int i = 0; i < numRows - 1; i++) {
+                for (int j = 0; j < numCols - 1; j++) {
+                    trimmedMatrix.setValue(i, j, inputMatrix.getValue(i, j));
+                }
+            }
+
+            return trimmedMatrix;
+        } else {
+            // If not all zeros in last row or last column, return the input matrix as is
+            return inputMatrix;
+        }
     }
 
 }
