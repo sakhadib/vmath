dbe7dde743a61fd57a3219e03dee99f263f6469f
sakhadib <adibsakhawat@iut-dhaka.edu>
2023-09-24 11:53:14 +0600

    Added Base Converter

---
+++ code/vmath/src/mathvoyage/BaseConverter.java
@@ -0,0 +1,4 @@
+package mathvoyage;
+
+public class BaseConverter {
+}
--- code/vmath/src/mathvoyage/Bitwise.java
+++ code/vmath/src/mathvoyage/Bitwise.java
@@ -1,5 +1,22 @@
 package mathvoyage;
 
 public class Bitwise {
+private static Bitwise instance;
+
+    private Bitwise() {
+        // Private constructor to prevent external instantiation
+    }
+
+    /**
+     * Returns the singleton instance of this class.
+     * @return The singleton instance
+     */
+    public static Bitwise getInstance() {
+        if (instance == null) {
+            instance = new Bitwise();
+        }
+        return instance;
+    }
+
 
 }
