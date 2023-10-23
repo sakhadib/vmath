cc2cd372287ce9bffd746e791ddf41eec4548b09
sakhadib <adibsakhawat@iut-dhaka.edu>
2023-10-23 13:45:29 +0600

    fixed an issue

--- code/vmath/src/mathvoyage/BaseConverter.java
+++ code/vmath/src/mathvoyage/BaseConverter.java
@@ -34,8 +34,9 @@ public class BaseConverter {
         int integerPart = 0;
         double fractionalPart = 0.0;
 
+        int partlength = parts[0].length();
         // Convert the integer part
-        for (int i = 0; i < parts[0].length(); i++) {
+        for (int i = 0; i < partlength; i++) {
             char digit = parts[0].charAt(i);
             if (digit == '1') {
                 integerPart = integerPart * 2 + 1;
