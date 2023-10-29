e6b68fe580b5818dae62a47c40b354f784f75fc0
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2023-10-29 20:41:28 +0600

    PolarPoint class created

--- code/vmath/src/mathvoyage/CartesianPoint.java
+++ code/vmath/src/mathvoyage/CartesianPoint.java
@@ -12,16 +12,34 @@ public class CartesianPoint implements Point{
         this.r = getR();
         this.theta = getTheta();
     }
-
+    /**
+     * Returns the x-coordinate of the point.
+     * @return The x-coordinate of the point
+     */
     public double getX(){
         return x;
     }
+    
+    /**
+     * Returns the y-coordinate of the point.
+     * @return The y-coordinate of the point
+     */
     public double getY(){
         return y;
     }
+
+    /**
+     * Returns the r-coordinate of the point.
+     * @return The r-coordinate of the point
+     */
     public double getR(){
         return vmath.algebra.sqrt(x*x + y*y);
     }
+
+    /**
+     * Returns the theta-coordinate of the point.
+     * @return The theta-coordinate of the point
+     */
     public double getTheta(){
         return vmath.Trigonometry.arctan(y/x, Angle.DEGREE);
     }
