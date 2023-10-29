5a4f01d4c0227c54a2efad68ed068a65ce58385a
sakhadib <adibsakhawat@iut-dhaka.edu>
2023-10-29 11:45:19 +0600

    matrix done _ 1

--- code/vmath/src/mathvoyage/Matrix.java
+++ code/vmath/src/mathvoyage/Matrix.java
@@ -5,9 +5,86 @@ package mathvoyage;
  *
  */
 public class Matrix{
-    private int[][] data;
+    private double[][] data;
     private int rows;
     private int cols;
+
+    public Matrix(double[][] data){
+        this.data = data;
+        this.rows = data.length;
+        this.cols = data[0].length;
+    }
+
+    public Matrix(int[][] data){
+        this.data = new double[data.length][data[0].length];
+        this.rows = data.length;
+        this.cols = data[0].length;
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[0].length; j++) {
+                this.data[i][j] = data[i][j];
+            }
+        }
+    }
+
+    public Matrix(double[] data, int rows, int cols){
+        if(data.length != rows*cols) {
+            throw new IllegalArgumentException("data length must be equal to rows*cols"
+                    + "\nrows: " + rows + "\ncols: " + cols + "\ndata length: " + data.length);
+        }
+        else {
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[rows][cols];
+            int k = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    this.data[i][j] = data[k];
+                    k++;
+                }
+            }
+        }
+    }
+
+    public Matrix(int[] data, int rows, int cols){
+        if(data.length != rows*cols) {
+            throw new IllegalArgumentException("data length must be equal to rows*cols"
+                    + "\nrows: " + rows + "\ncols: " + cols + "\ndata length: " + data.length);
+        }
+        else {
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[rows][cols];
+            int k = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++){
+                    this.data[i][j] = data[k];
+                    k++;
+                }
+            }
+        }
+    }
+
+    public Matrix(int rows, int cols, double value){
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new double[rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                this.data[i][j] = value;
+            }
+        }
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public int getCols(){
+        return cols;
+    }
+
+
+
 
 
 }
