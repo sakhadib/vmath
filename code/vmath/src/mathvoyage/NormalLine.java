96551b0d33e3b917be52ece193da75de1cf49430
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2023-10-29 12:51:27 +0600

    Javadoc of SlopeLine SegmentLine NormalLine

--- code/vmath/src/mathvoyage/NormalLine.java
+++ code/vmath/src/mathvoyage/NormalLine.java
@@ -18,24 +18,59 @@ public class NormalLine implements Line{
         this.yIntercept = getYIntercept();
         this.xIntercept = getXIntercept();
     }
+
+    /**
+     * Returns the coefficient of x of the line.
+     * @return The coefficient of x of the line
+     */
     public double getCoefficientOfX() {
         return coefficientOfX;
     }
+
+    /**
+     * Returns the coefficient of y of the line.
+     * @return The coefficient of y of the line
+     */
     public double getCoefficientOfY() {
         return coefficientOfY;
     }
+
+    /**
+     * Returns the constant of the line.
+     * @return The constant of the line
+     */
     public double getConstant() {
         return constant;
     }
+
+    /**
+     * Returns the slope of the line.
+     * @return The slope of the line
+     */
     public double getSlope() {
         return -coefficientOfX/coefficientOfY;
     }
+
+    /**
+     * Returns the shift of the line.
+     * @return The shift of the line
+     */
     public double getShift() {
         return -constant/coefficientOfY;
     }
+
+    /**
+     * Returns the y-intercept of the line.
+     * @return The y-intercept of the line
+     */
     public double getYIntercept() {
         return constant/coefficientOfY;
     }
+
+    /**
+     * Returns the x-intercept of the line.
+     * @return The x-intercept of the line
+     */
     public double getXIntercept() {
         return constant/coefficientOfX;
     }
--- code/vmath/src/mathvoyage/SlopeLine.java
+++ code/vmath/src/mathvoyage/SlopeLine.java
@@ -19,30 +19,64 @@ public class SlopeLine implements Line{
         this.coefficientOfY = getCoefficientOfY();
         this.constant = getConstant();
     }
+    /**
+     * Returns the slope of the line.
+     * @return The slope of the line
+     */
     public double getSlope() {
         return slope;
     }
+
+    /**
+     * Returns the shift of the line.
+     * @return The shift of the line
+     */
     public double getShift() {
         return shift;
     }
+
+    /**
+     * Returns the x-intercept of the line.
+     * @return The x-intercept of the line
+     */
     public double getXIntercept() {
         if (shift == 0) {
             return 0;
         }
         return -shift * slope;
     }
+
+    /**
+     * Returns the y-intercept of the line.
+     * @return The y-intercept of the line
+     */
     public double getYIntercept(){
         if(shift == 0){
             return 0;
         }
         return shift;
     }
+
+    /**
+     * Returns the coefficient of x of the line.
+     * @return The coefficient of x of the line
+     */
     public double getCoefficientOfX(){
         return slope;
     }
+
+    /**
+     * Returns the coefficient of y of the line.
+     * @return The coefficient of y of the line
+     */
     public double getCoefficientOfY(){
         return -1;
     }
+
+    /**
+     * Returns the constant of the line.
+     * @return The constant of the line
+     */
     public double getConstant(){
         return shift;
     }
