558cd1835f4ccb8ad724ce95a3ff16db67013c72
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2023-10-29 15:52:02 +0600

    point and geometry class created

---
+++ code/vmath/src/mathvoyage/Geometry.java
@@ -0,0 +1,23 @@
+package mathvoyage;
+/**
+ * voyager.geometry.java
+ * @author Tahsin Islam
+ */
+public class Geometry {
+    private static Geometry instance;
+    private Geometry() {
+        // Private constructor to prevent external instantiation
+    }
+    /**
+     * Returns the singleton instance of this class.
+     * @return The singleton instance
+     */
+    public static Geometry getInstance() {
+        if (instance == null) {
+            instance = new Geometry();
+        }
+        return instance;
+    }
+
+
+}
