71d7fcee1f1bc61a09f850f32066dd5268a06960
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2023-10-29 11:19:18 +0600

    interface created

---
+++ code/vmath/src/mathvoyage/Line.java
@@ -0,0 +1,11 @@
+package mathvoyage;
+
+public interface Line {
+    double getSlope();
+    double getShift();
+    double getYIntercept();
+    double getXIntercept();
+    double getCoefficientOfX();
+    double getCoefficientOfY();
+    double getConstant();
+}
