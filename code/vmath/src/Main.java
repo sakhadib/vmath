1c59783f884152d7ed85edf41993f0ed750f30e9
takiafarhin <takiafarhin@iut-dhaka.edu>
2023-11-07 15:06:13 +0600

    AND operation done

--- code/vmath/src/Main.java
+++ code/vmath/src/Main.java
@@ -154,6 +154,8 @@ public class Main {
 
 
 
+
+
     }
 
 
--- code/vmath/src/mathvoyage/bitwise/Bitwise.java
+++ code/vmath/src/mathvoyage/bitwise/Bitwise.java
@@ -28,7 +28,7 @@ private static Bitwise instance;
      * @param b is the int value given
      * @return or value
       */
-    public static int bitwiseOR(int a, int b) {
+    public  int bitwiseOR(int a, int b) {
         int result = 0;
         int mask = 1;
 
@@ -45,23 +45,36 @@ private static Bitwise instance;
     }
 
 
+    /**
+     * Return the bitwise And value
+     * @param a is the given value
+     * @param b is the given value
+     * @return AND value
+     */
+    public  int BitWiseAnd(int a, int b) {
+
+        int result = 0;
+        int mask = 1;
+
+        for (int i=0; i<32;i++) // assuming 32-bit integer
+        {
+
+            int bin1 = (a & mask) >> i;
+            int bin2 = (b & mask) >> i;
 
 
-   
+            int bitWiseANDResult = bin1 & bin2;
+
+            result |= (bitWiseANDResult << i);
+
+            mask <<=1;
+
+        }
 
 
+        return  result;
 
-
-
-
-
-
-
-
-
-//    public class BitWiseAnd(String bin1, String bin2){
-//
-//    }
+    }
 
 
 
